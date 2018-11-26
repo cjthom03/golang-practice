0cc61eab060ae99e3690ccd90e05e9423304acdf
Charles Thomas <cjthom03@gmail.com>
2018-11-25 17:11:10 -0800

    Add variables.go file

---
+++ examples/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a = "Start with vars"
+  var b, c = 1, 2
+  var d = true
+  var e int
+
+  f := "Then use :="
+  g, h := 3, 4
+  i := false
+  var j bool
+  var k string
+
+  fmt.Println(a)
+  fmt.Println(b, c)
+  fmt.Println(d)
+  fmt.Println(e)
+
+  fmt.Println(f)
+  fmt.Println(g, h)
+  fmt.Println(i)
+  fmt.Println(j)
+  fmt.Println(k)
+}
